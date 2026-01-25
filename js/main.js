e5deb66edf91e605df52f9b7606837a5caffdd68
どーぷ <konno5423m@gmail.com>
2026-01-26 08:29:15 +0900

    14

--- js/main.js
+++ js/main.js
@@ -5,3 +5,21 @@ AOS.init({
     easing: 'ease-in',
     once: true,
 });
+
+const navButton = document.querySelector('.header__navButton');
+const navList = document.querySelector('.header__navList');
+
+navButton.addEventListener('click', () => {
+  navList.classList.toggle('is-open');
+  navButton.classList.toggle('is-active');
+});
+
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY < 50) {
+    header.classList.add('is-transparent');
+  } else {
+    header.classList.remove('is-transparent');
+  }
+});
