7674b7628c6b9b57e1891c4db02ba4339e57a428
どーぷ <konno5423m@gmail.com>
2026-01-26 23:27:25 +0900

    15

--- js/main.js
+++ js/main.js
@@ -6,20 +6,26 @@ AOS.init({
     once: true,
 });
 
-const navButton = document.querySelector('.header__navButton');
-const navList = document.querySelector('.header__navList');
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('.header__navButton');
+  const navList = document.querySelector('.header__navList');
 
-navButton.addEventListener('click', () => {
-  navList.classList.toggle('is-open');
-  navButton.classList.toggle('is-active');
-});
+  console.log(button, navList);
 
-const header = document.querySelector('.header');
+  if (!button || !navList) return;
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY < 50) {
-    header.classList.add('is-transparent');
-  } else {
-    header.classList.remove('is-transparent');
-  }
+  button.addEventListener('click', () => {
+    button.classList.toggle('is-active');
+    navList.classList.toggle('is-open');
+  });
+  const header = document.querySelector(".header");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      header.classList.remove("is-transparent");
+    } else {
+      header.classList.add("is-transparent");
+    }
+  });
+  
 });
